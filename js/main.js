70ab439b26b5eea265c1dc3922fc10c1f3c11b40
Carlos <jjbbpe@gmail.com>
2020-09-07 16:55:06 +0200

    casi mapa

--- js/main.js
+++ js/main.js
@@ -3,10 +3,7 @@
 
 var regalo = document.getElementById('regalo');
 
-
-
   document.addEventListener('DOMContentLoaded', function(){
-    console.log("listo");
 
       //Campos Datos Usuario
       var nombre = document.getElementById('nombre');
@@ -15,22 +12,92 @@ var regalo = document.getElementById('regalo');
 
       //Campos Pases
       var pase_dia = document.getElementById('pase_dia');
-      var pase_dosdias = document.getElementById('pase_dosdias');
+      var pase_dosdias = document.getElementById('pase_2_dias');
       var pase_completo = document.getElementById('pase_completo');
 
       //Botones y divs
       var calcular = document.getElementById('calcular');
       var errorDiv = document.getElementById('errorDiv');
       var botonRegistro = document.getElementById('btnRegistro');
-      var resultado = document.getElementById('lista-productos');
+      var lista_productos = document.getElementById('lista-productos');
+      var suma = document.getElementById('suma-total');
+
+      //EXTRAS
+      var camisas = document.getElementById('camisa_evento')
+      var etiquetas = document.getElementById('etiquetas')
 
       calcular.addEventListener('click',calcularMontos);
+      pase_dia.addEventListener('blur',mostrarDias);
+      pase_dosdias.addEventListener('blur',mostrarDias);
+      pase_completo.addEventListener('blur',mostrarDias);
+
 
 
       function calcularMontos(event){
         event.preventDefault();
-        console.log("has hecho un click");
+        if(regalo.value === ""){
+          alert('debes elegir un regalo')
+          regalo.focus();
+        }else{
+          var boletosDia = parseInt(pase_dia.value , 10) || 0,
+              boletos2Dias = parseInt(pase_2_dias.value, 10) || 0,
+              boletoCompleto = parseInt(pase_completo.value, 10) || 0,
+              cantCamisas = parseInt(camisas.value, 10) || 0,
+              cantEtiquetas = parseInt(etiquetas.value, 10) || 0;
+
+              var totalPagar = (boletosDia*30) + (boletos2Dias*45) + (boletoCompleto*50) + ((cantCamisas*10) * .93) + (cantEtiquetas * 2)
+
+              var listadoProductos = [];
+
+              if (boletosDia>=1) {
+                listadoProductos.push(boletosDia + ' Pases por día');
+              }
+              if (boletos2Dias>=1) {
+                listadoProductos.push(boletos2Dias + ' Pases de  2 días');
+              }
+              if (boletoCompleto>=1) {
+                listadoProductos.push(boletoCompleto + ' Pases completos');
+              }
+              if (cantCamisas>=1) {
+                listadoProductos.push(cantCamisas + ' camisas');
+              }
+              if (cantEtiquetas>=1) {
+                listadoProductos.push(cantEtiquetas + ' etiquetas');
+              }
+
+              lista_productos.style.display = "block";
+              lista_productos.innerHTML = '';
+
+              for( var i=0; i<listadoProductos.length; i++){
+                lista_productos.innerHTML += listadoProductos[i] + "<br/>";
+              }
+
+              suma.innerHTML = "$ " + totalPagar.toFixed(2);
+        }
       }
 
-  }); //DOM CONTENT LOADED
+      function mostrarDias() {
+        var boletosDia = parseInt(pase_dia.value , 10) || 0,
+            boletos2Dias = parseInt(pase_2_dias.value,10) || 0,
+            boletoCompleto = parseInt(pase_completo.value, 10) || 0;
+
+            var diasElegidos = [];
+
+            if(boletosDia > 0){
+              diasElegidos.push('viernes');
+            }
+            if(boletos2Dias > 0){
+              diasElegidos.push('viernes', 'sabado');
+            }
+            if(boletoCompleto > 0){
+              diasElegidos.push('viernes', 'sabado', 'domingo');
+            }
+
+            for(var i= 0; i < diasElegidos.length; i++){
+              document.getElementById(diasElegidos[i]).style.display = "block";
+            }
+      }
+
+
+    }); //DOM CONTENT LOADED
   })();
