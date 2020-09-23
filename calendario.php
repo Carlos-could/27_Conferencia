a79b37cff3832947c285e7b49a26ec88e68ffb4b
Carlos <jjbbpe@gmail.com>
2020-09-23 18:16:40 +0200

    php calender

--- calendario.php
+++ calendario.php
@@ -10,7 +10,7 @@
       //creamos la conexion
       require_once('includes/funciones/bd_conexion.php');
       //escribimos la consulta
-      $sql = "SELECT evento_id, nombre_evento, fecha_evento, hora_evento, cat_evento, nombre_invitado, apellido_invitado ";
+      $sql = "SELECT evento_id, nombre_evento, fecha_evento, hora_evento, cat_evento, icono, nombre_invitado, apellido_invitado ";
       $sql .= ' FROM eventos';
       $sql .= ' INNER JOIN categoria_evento';
       $sql .= ' ON eventos.id_cat_evento = categoria_evento.id_categoria';
@@ -42,6 +42,7 @@
             'fecha' => $eventos['fecha_evento'],
             'hora' => $eventos['hora_evento'],
             'categoria' => $eventos['cat_evento'],
+            'icono' => $eventos['icono'],
             'invitado' => $eventos['nombre_invitado'] . " " . $eventos['apellido_invitado']
         );
 
@@ -50,9 +51,41 @@
 
     <?php } // while de fetch_assoc() ?>
 
-    <pre>
-      <?php var_dump($calendario); ?>
-    </pre>
+    <?php
+      //imprimir todos los Eventos
+      foreach ($calendario as $dia => $lista_eventos) { ?>
+        <h3>
+          <i class="fas fa-calendar-alt"></i>
+
+          <?php
+          //Unix
+          setlocale(LC_TIME, 'es_ES.UTF-8');
+          //Windows
+          setlocale(LC_TIME, 'spanish');
+          echo strftime("%A, %d de %B del %Y", strtotime($dia) ); ?>
+        </h3>
+        <?php foreach ($lista_eventos as $evento) { ?>
+            <div class="dia">
+              <p class="titulo"> <?php echo $evento['titulo']; ?></p>
+              <p class="hora">
+                <i class="far fa-clock-o" aria-hidden="true"></i>
+
+                <?php echo $evento['fecha'] . " " . $evento['hora']; ?>
+              </p>
+              <p>
+                <i class="fas fa-landmark"></i>
+                <?php echo $evento['categoria']; ?></p>
+              <p>
+                <i class="far fa-user" aria-hidden="true"></i>
+                <?php echo $evento['invitado']; ?>
+              </p>
+              
+            </div>
+        <?php } // fin foreach eventos?>
+
+      <?php } // fin foreach de dias?>
+
+
 
 
   </div>
