d7303318b4625536e15cd9b01ce23985a678488c
Carlos-could <jjbbpe@gmail.com>
2020-12-12 15:01:40 +0100

    menu admin color

--- admin/templates/navegacion.php
+++ admin/templates/navegacion.php
@@ -4,18 +4,16 @@
   <section class="sidebar">
    <!-- Sidebar user panel -->
    <div class="user-panel">
-      <div class="pull-left image">
-        <img src="../../dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-      </div>
-      <div class="pull-left info">
-        <p>Alexander Pierce</p>
+
+      <div class="info">
+        <p>Carlos Moran</p>
         <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
       </div>
    </div>
    <!-- search form -->
    <form action="#" method="get" class="sidebar-form">
       <div class="input-group">
-        <input type="text" name="q" class="form-control" placeholder="Search...">
+        <input type="text" name="q" class="form-control" placeholder="Buscar...">
         <span class="input-group-btn">
               <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
               </button>
@@ -25,7 +23,7 @@
    <!-- /.search form -->
    <!-- sidebar menu: : style can be found in sidebar.less -->
    <ul class="sidebar-menu" data-widget="tree">
-      <li class="header">MAIN NAVIGATION</li>
+      <li class="header">Menu de Administración</li>
       <li class="treeview">
         <a href="#">
           <i class="fa fa-dashboard"></i> <span>Dashboard</span>
