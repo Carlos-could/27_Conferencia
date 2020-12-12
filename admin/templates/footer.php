e7e93320dd98be5b0c66342b81079d54e78f1e9d
Carlos-could <jjbbpe@gmail.com>
2020-12-12 13:02:43 +0100

    admin2

--- admin/templates/footer.php
+++ admin/templates/footer.php
@@ -1,21 +1,7 @@
 <footer class="main-footer">
-  <div class="float-right d-none d-sm-block">
-   <b>Version</b> 3.1.0-rc
+  <div class="pull-right hidden-xs">
+   <b>Version</b> 2.4.13
   </div>
-  <strong>Copyright &copy; 2014-2020 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+  <strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE</a>.</strong> All rights
+  reserved.
 </footer>
-
-
-</div>
-<!-- ./wrapper -->
-
-<!-- jQuery -->
-<script src="js/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="js/demo.js"></script>
-</body>
-</html>
