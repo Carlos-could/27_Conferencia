9ff0b60fc53986229e641eecae774711eb2cc46f
Carlos-could <jjbbpe@gmail.com>
2020-12-15 00:22:59 +0100

    445 y 446 fertig

--- admin/insertar-admin.php
+++ admin/insertar-admin.php
@@ -1,9 +1,8 @@
 <?php
 
-     include_once 'funciones/funciones.php';
-
-
      if (isset($_POST['agregar-admin'])) {
+
+
           $usuario = $_POST['usuario'];
           $nombre = $_POST['nombre'];
           $password = $_POST['password'];
@@ -13,6 +12,32 @@
           );
 
           $password_hashed = password_hash($password, PASSWORD_BCRYPT, $opciones);
+
+          try {
+              include_once 'funciones/funciones.php';
+              $stmt = $conn->prepare("INSERT INTO admins (usuario, nombre, password) VALUES (?,?,?)");
+              $stmt->bind_param("sss", $usuario, $nombre, $password_hashed);
+              $stmt->execute();
+              $id_registro = $stmt->insert_id;
+
+              if ($id_registro > 0) {
+                  $respuesta = array(
+                      'respuesta' => 'exito',
+                      'id_admin' => $id_registro
+                  );
+              } else {
+                  $respuesta = array(
+                      'respuesta' => 'error'
+                  );
+              }
+              $stmt->close();
+              $conn->close();
+
+          } catch (Exception $e) {
+              echo "Error: " . $e->getMessage();
+          }
+
+          die(json_encode($respuesta));
 
      }
  ?>
