a73e8a23325a762bf42c087d67373a91afcbcc00
Carlos-could <jjbbpe@gmail.com>
2020-12-15 16:30:32 +0100

    bis 449 fertig

--- admin/admin-area.php
+++ admin/admin-area.php
@@ -1,7 +1,8 @@
 <?php
-include_once 'templates/header.php';
-include_once 'templates/barra.php';
-include_once 'templates/navegacion.php';
+    include_once 'funciones/funciones.php';
+    include_once 'templates/header.php';
+    include_once 'templates/barra.php';
+    include_once 'templates/navegacion.php';
 
  ?>
 
@@ -28,7 +29,7 @@ include_once 'templates/navegacion.php';
         <div class="box-header with-border">
           <h3 class="box-title">Title</h3>
 
-          
+
         </div>
         <div class="box-body">
           Start creating your amazing application!
--- admin/js/admin-ajax.js
+++ admin/js/admin-ajax.js
@@ -28,4 +28,35 @@ $(document).ready(function() {
         })
 
     });
+
+    $('#login-admin').on('submit', function(e) {
+        e.preventDefault();
+
+        var datos = $(this).serializeArray();
+
+        $.ajax({
+            type: $(this).attr('method'),
+            data: datos,
+            url: $(this).attr('action'),
+            dataType: 'json',
+            success: function(data){
+                console.log(data);
+                var resultado = data;
+                if (resultado.respuesta == 'exitoso') {
+                    swal(
+                      'Login Correcto!',
+                      'Bienvenid@ '+resultado.usuario+' !! ',
+                      'success'
+                    )
+                } else {
+                    swal (
+                        'Error!',
+                        'Usuario o Password incorrecto!',
+                        'error'
+                    )
+                }
+            }
+        })
+
+    });
 });
